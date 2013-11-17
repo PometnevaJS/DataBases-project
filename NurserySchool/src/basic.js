5015da7d0bd462b1721cab77848983fff5a9d790
PometnevaJS <358225705@mail.ru>
2013-11-17 17:07:19 +0400

    Интерфейс

---
+++ NurserySchool/src/basic.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @author User
+ * @name basic
+ */
+
+function НайтиActionPerformed(evt) {//GEN-FIRST:event_НайтиActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_НайтиActionPerformed
+
+function DeleteActionPerformed(evt) {//GEN-FIRST:event_DeleteActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_DeleteActionPerformed
+
+function Поиск по воспитанникамActionPerformed(evt) {//GEN-FIRST:event_Поиск по воспитанникамActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_Поиск по воспитанникамActionPerformed
+
+function checkBoxActionPerformed(evt) {//GEN-FIRST:event_checkBoxActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_checkBoxActionPerformed
+
+function button7ActionPerformed(evt) {//GEN-FIRST:event_button7ActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_button7ActionPerformed
+
+function textField4ActionPerformed(evt) {//GEN-FIRST:event_textField4ActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_textField4ActionPerformed
+
+function button29ActionPerformed(evt) {//GEN-FIRST:event_button29ActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_button29ActionPerformed
